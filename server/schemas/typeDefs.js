59e6d6ef7fe9c12cc13af801087d41feefa3c3b3
BVanBuren <bvanburenwx@gmail.com>
2021-10-26 13:47:53 -0400

    Added the typeDefs.js and completed it, updated package.json on server side to include apollo and ran npm i to install it

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,46 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Book {
+        authors: [String]
+        description: String
+        bookID: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input savedBook {
+        bookId: String
+        authors: [String]
+        description: String 
+        image: String
+        link: String
+        title: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: savedBook!): User
+        removeBook(bookId: ID!); User
+    }
+`;
+
+module.exports = typeDefs;
